39f968548dd0f2f70f8c517b3f944804359ca020
17722077220 <2424005179@qq.com>
2024-08-16 09:05:05 +0800

    feat: initWeather-render

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -17,23 +17,81 @@ class Home extends Component {
     initWeather(city) {
         console.log(city, 'city')
         let _self = this
-        AMap.plugin('AMap.Weather', function() {
+        window.AMap.plugin('AMap.Weather', function () {
+            var weather = new AMap.Weather()
+            // 实时天气查询
+            weather.getLive(city, (err, data) => {
+                console.log(err, data, 'err, data')
+                _self.props.getWeather(data)
+            })
+            weather.getForecast(city, (err, data) => {
 
+            })
         })
+    }
+
+    initEchart(array) {
+        let domChart = this.dom
+        var myChart = echarts.init(domChart)
+        let option = null
+        option = {
+            title: {
+                show: true,
+                text: '天气变化趋势',
+                x: 'center',
+                textStyle: {
+                    color: 'grey'
+                },
+                padding: [5, 0, 30, 10]
+            },
+            xAxis: {
+                show: false,
+                type: 'category',
+                axisLine: {
+                    lineStyle: {
+                        color: '#fff'
+                    }
+                },
+                grid: { bottom: "20" }
+            },
+            yAxis: {
+                show: false
+            },
+            series: [
+                {
+                    data: array,
+                    type: "line",
+                    itemStyle: {
+                        normal:
+                        {
+                            label: {
+                                show: true,
+                                formatter: "{c}℃"
+                            },
+                            lineStyle: {
+                                color: 'white' //改变折线颜色
+                            },
+                            color: '#eee'
+                        },
+                    }
+                }
+            ]
+        }
+        myChart.setOption(option, true)
     }
     // mapState 函数定义了如何从 Redux store 的 state 中提取数据，并将这些数据映射到组件的 props 上。在你的 mapState 函数中，你从 Redux store 的 state 中提取了 city、weatherData、forecast 和 init 这些数据，并将它们作为对象的属性返回
     // 使用 connect(mapState, mapDispatch) 将 mapState 和 mapDispatch 与组件 Home 连接起来时，这些属性会被注入到 Home 组件的 props 中
     componentDidMount() {
         let _self = this
-        if(_self.props.init) {
-            console.log(_self, '_self')
+        if (_self.props.init) {
+            // console.log(_self, '_self')
             // console.log(window.AMap, 'window.AMap')
-            window.AMap.plugin('AMap.CitySearch', function() {
+            window.AMap.plugin('AMap.CitySearch', function () {
                 var citySearch = new window.AMap.CitySearch()
-                // console.log(citySearch, 'citySearch')
-                citySearch.getLocalCity(function(status, result) {
-                    // console.log(status, result, 'getLocalCity')
-                    if(status === 'complete' && result.info === 'OK') {
+                console.log(citySearch, 'citySearch')
+                citySearch.getLocalCity(function (status, result) {
+                    console.log(status, result, 'getLocalCity')
+                    if (status === 'complete' && result.info === 'OK') {
                         _self.props.getCity(result.city)
                         _self.initWeather(_self.props.city)
                         _self.props.getInit()
